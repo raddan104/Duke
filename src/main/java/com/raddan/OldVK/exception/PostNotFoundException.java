295f136b6bf5953b68867b8af104b574232bb2fd
raddan104 <skyraddan@outlook.com>
2024-06-25 15:26:49 +0300

    Exception updates

--- src/main/java/com/raddan/OldVK/exception/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.raddan.OldVK.exception;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/raddan/OldVK/exception/custom/UserNotFoundException.java
+++ src/main/java/com/raddan/OldVK/exception/custom/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.raddan.OldVK.exception;
+package com.raddan.OldVK.exception.custom;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
