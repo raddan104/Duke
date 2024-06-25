fb22de9c402c72b49776c0629fe42e1bda107fd7
raddan104 <skyraddan@outlook.com>
2024-06-25 15:26:28 +0300

    Minor repositories updates

--- src/main/java/com/raddan/OldVK/repository/PostRepository.java
+++ src/main/java/com/raddan/OldVK/repository/PostRepository.java
@@ -1,8 +1,6 @@
 package com.raddan.OldVK.repository;
 
 import com.raddan.OldVK.entity.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +10,4 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<List<Post>> findAllByAuthorId(Long authorId);
-    Page<Post> findAllByAuthorIdOrderByCreatedAtDesc(Long authorId, Pageable pageable);
-    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
-    Long countByAuthorId(Long authorId);
 }
